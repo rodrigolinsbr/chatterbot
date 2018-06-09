49142775229ede1689ea853924ec3065712d2c69
System Administrator <root@MacBook-Pro-de-SABER.local>
2018-06-09 01:02:25 -0300

    aprimorando algoritmo

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
-from  chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ListTrainer
 from chatterbot import  ChatBot
+from corpus import student, introdution, lingPython
 
 bot = ChatBot('Teste')
-convI = ['oi', 'olá','olá, bom dia!', 'Bom dia como vai?','Estou bem']
-convF = ['Qual seu nome?','Meu nome é Sabidão', 'Você é aluno?','Já conhece nossos curso?','Não?','Acesse: www.sabertecnologias.com' ]
-convE = ['Estou com problemas','Você tem problemas com Elabore?','O que achou do sistema?', 'Eu gostei bastante!', 'Você conhece Isabela?', 'Qual o browser que você usa?']
-
 bot.set_trainer(ListTrainer)
-bot.train(convI)
-bot.train(convF)
-bot.train(convE)
-
+bot.train(introdution.trainIntrodution)
+bot.train(student.trainStudent)
+bot.train(lingPython.trainStudent)
+cont = 0
 while True:
+
     quest = input('Você: ')
     response = bot.get_response(quest)
     if float(response.confidence)>0.5:
         print ('Bot: ', response)
     else:
-        print('Bot: Não entendi')
+        cont = cont + 1
+        if cont==1:
+            print('Bot: Não entendi')
+        elif cont==2:
+            print('Bot: Desculpe, não estou entendo.')
+        elif cont>2:
+            print('Bot: Caramba, não entendo.')
+
+
+
+
